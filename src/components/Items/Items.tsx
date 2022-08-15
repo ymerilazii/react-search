ce0a89bf482b9c3a7d7c6332239957897fe27571
unknown <ymerilazii@gmail.com>
2022-08-15 14:50:40 +0200

    initial commit

---
+++ src/components/Items/Items.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+interface Props{
+    items: object,
+}
+export const Items = (props: Props) => {
+  return (
+    <div>Items</div>
+  )
+}
---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SearchInput = () => {
+  return (
+    <input type="text" />
+  )
+}
